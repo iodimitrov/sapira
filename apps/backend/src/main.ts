5e9732189e09fcaefd10911482e4b233873ba3ed
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2025-01-19 17:11:08 +0200

    feat: add cookie parser

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    rawBody: true,
+  });
+
+  const configService = app.get(ConfigService);
+
+  const cookieSecret = configService.getOrThrow<string>('COOKIE_SECRET');
+  app.use(cookieParser([cookieSecret]));
+
   await app.listen(3001);
 }
 bootstrap();
