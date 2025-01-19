a71c01fc88e1cbe1147f421b22f63116cd191579
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2025-01-20 01:31:39 +0200

    feat: add nodemailer with ejs

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConfig } from '@sapira/database';
 import { UserModule } from './user/user.module';
 import { InstitutionModule } from './institution/institution.module';
 import { BaseAuthModule } from '@sapira/nest-common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 @Module({
   imports: [
@@ -16,6 +18,30 @@ import { BaseAuthModule } from '@sapira/nest-common';
     }),
     TypeOrmModule.forRootAsync({
       useFactory: () => getConfig(),
+    }),
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.getOrThrow<string>('SMTP_HOST'),
+          port: +configService.getOrThrow<string>('SMTP_PORT'),
+          secure: configService.getOrThrow<string>('SMTP_SECURE') === 'true',
+          auth: {
+            user: configService.getOrThrow<string>('SMTP_USER'),
+            pass: configService.getOrThrow<string>('SMTP_PASS'),
+          },
+        },
+        defaults: {
+          from: configService.getOrThrow<string>('SMTP_DEFAULT_FROM'),
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new EjsAdapter(),
+          options: {
+            strict: false, // Disables usage of `locals` for EJS
+          },
+        },
+      }),
     }),
     BaseAuthModule,
     InstitutionModule,
