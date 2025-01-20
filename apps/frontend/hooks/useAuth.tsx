cdaea3bf0bcfeb801ae1bf3259281bc0f73dcdbb
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2025-01-20 12:40:01 +0200

    feat: add useAuth hook

---
+++ apps/frontend/hooks/useAuth.tsx
@@ -0,0 +1,94 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { gql } from 'graphql-request';
+import gqlClient from '../client';
+import { User } from '@sapira/database';
+
+type AuthStatus = 'FETCHING' | 'DONE' | 'REDIRECT';
+
+type Auth = {
+  user: User | null;
+  status: AuthStatus;
+  logout: () => void;
+};
+
+export const useAuth = (): Auth => {
+  const router = useRouter();
+  const [user, setUser] = useState<{ data: User | null; status: AuthStatus }>({
+    data: null,
+    status: 'FETCHING',
+  });
+
+  const logout = async () => {
+    await gqlClient.request(gql`
+      query {
+        logout
+      }
+    `);
+    setUser({
+      data: null,
+      status: 'REDIRECT',
+    });
+    router.push('/');
+  };
+
+  useEffect(() => {
+    (async () => {
+      const data = await gqlClient.request<{ checkRefreshToken: boolean }>(gql`
+        query {
+          checkRefreshToken
+        }
+      `);
+      if (data.checkRefreshToken) {
+        const query = gql`
+          query {
+            getProfile {
+              id
+              firstName
+              middleName
+              lastName
+              email
+              role
+              status
+              institution {
+                id
+                name
+                email
+                type
+                educationalStage
+                alias
+              }
+            }
+          }
+        `;
+        try {
+          const user = await gqlClient.request<{ getProfile: User }>(query);
+          setUser({
+            data: user.getProfile,
+            status: 'DONE',
+          });
+        } catch {
+          await gqlClient.request(gql`
+            query {
+              token {
+                accessToken
+              }
+            }
+          `);
+          const user = await gqlClient.request<{ getProfile: User }>(query);
+          setUser({
+            data: user.getProfile,
+            status: 'DONE',
+          });
+        }
+      } else {
+        setUser({
+          data: null,
+          status: 'REDIRECT',
+        });
+      }
+    })();
+  }, []);
+
+  return { user: user[0], status: user[1], logout };
+};
