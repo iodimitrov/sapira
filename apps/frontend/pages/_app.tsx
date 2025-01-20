16f1b750d6b49a6fb4ef2664b41172c40d3436f1
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2025-01-20 16:09:22 +0200

    fix: fix SWR fetcher format

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <StylesProvider injectFirst>
           <SWRConfig
             value={{
-              fetcher: (query: string, variables?: any) =>
+              fetcher: ([query, variables]: [string, any]) =>
                 gqlClient.request(query, variables),
             }}
           >
