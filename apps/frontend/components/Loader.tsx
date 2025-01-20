77e4f46e43691ff444c7b3563594f1a4f81c4bf7
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2025-01-20 12:39:51 +0200

    feat: add Loader component

---
+++ apps/frontend/components/Loader.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from 'react';
+import { CircularProgress, Container } from '@material-ui/core';
+import styles from 'styles/Loader.module.scss';
+
+const Loader: FunctionComponent = () => {
+  return (
+    <>
+      <Container maxWidth={false} className={styles.content} disableGutters>
+        <CircularProgress />
+      </Container>
+    </>
+  );
+};
+
+export default Loader;
