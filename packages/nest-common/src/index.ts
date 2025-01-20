530966f0a2510345647907b9703b357d8e4acf69
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2025-01-21 00:04:29 +0200

    feat: remove unused custom upload scalar

--- packages/nest-common/src/index.ts
+++ packages/nest-common/src/index.ts
@@ -6,7 +6,6 @@ export { CurrentUser } from './base-auth/decorators/current-user.decorator';
 export { GqlAuthGuard } from './base-auth/guards/gql-auth.guard';
 
 export { DateScalar } from './scalars/date.scalar';
-export { UploadScalar } from './scalars/upload.scalar';
 
 export { R2Service } from './cloudflare/r2.service';
 export { R2File } from './cloudflare/r2-file.type';
